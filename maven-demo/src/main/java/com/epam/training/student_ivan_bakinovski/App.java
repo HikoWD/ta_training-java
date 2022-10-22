aeda62796b2f702fce539ed40dcf8372b0f3b75e
HikoWD <hikowde@gmail.com>
2022-10-23 00:21:50 +0300

    maven

---
+++ maven-demo/src/main/java/com/epam/training/student_ivan_bakinovski/App.java
@@ -0,0 +1,13 @@
+package com.epam.training.student_ivan_bakinovski;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
